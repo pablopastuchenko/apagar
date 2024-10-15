579fc9fe59a1f6f6c660377e30879c005c70c22a
pastuchenko <pablopastuchenkooo@gmail.com>
2024-10-15 11:02:14 -0300

    first

--- app/lib/auth.ts
+++ app/lib/auth.ts
@@ -4,6 +4,13 @@ import type { NextAuthOptions } from "next-auth";
 import GithubProvider from "next-auth/providers/github";
 import { prisma } from "./db";
 
+interface NextAuthUserWithStringId {
+  id: string;
+  name?: string | null;
+  email?: string | null;
+  image?: string | null;
+}
+
 export const authOptions: NextAuthOptions = {
   // @ts-ignore
   adapter: PrismaAdapter(prisma),
@@ -11,6 +18,15 @@ export const authOptions: NextAuthOptions = {
     GithubProvider({
       clientId: process.env.GITHUB_CLIENT_ID as string,
       clientSecret: process.env.GITHUB_SECRET_ID as string,
+      profile(profile) {
+        return {
+          id: profile.id.toString(),
+          name: profile.name || profile.login,
+          email: profile.email,
+          image: profile.avatar_url,
+        } as NextAuthUserWithStringId;
+      },
     }),
   ],
+  // Adicione outras configurações aqui, como secret, se necessário
 };
